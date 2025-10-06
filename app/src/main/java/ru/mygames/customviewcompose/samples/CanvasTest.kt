46e857db09ff26538bec02c8261711ef95bbb19c
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-10-06 10:58:39 +0300

    понял, что не стоит использовать пиксили, сначала использую dp, потом перевожу в пиксели

--- app/src/main/java/ru/mygames/customviewcompose/samples/CanvasTest.kt
+++ app/src/main/java/ru/mygames/customviewcompose/samples/CanvasTest.kt
@@ -7,6 +7,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CanvasTest(){
@@ -17,8 +20,13 @@ fun CanvasTest(){
     ) {
         drawLine(
             color = Color.White,
-            start = Offset(x = 100f, y = 100f),
-            end = Offset(x = 200f, y = 200f)
+            start = Offset(x = 0f, y = 0f),
+            end = Offset(x = this.size.width, y = this.size.height)
+        )
+        drawCircle(
+            color = Color.White,
+            radius = 100.dp.toPx(),
+            style = Stroke(width = 10.dp.toPx())
         )
     }
 }
