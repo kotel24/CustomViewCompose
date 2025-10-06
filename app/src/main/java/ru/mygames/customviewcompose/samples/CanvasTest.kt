40250a52628a9ed89c6b6a636743924a6791db60
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-10-06 11:06:31 +0300

    решение дз написал имя олег

--- app/src/main/java/ru/mygames/customviewcompose/samples/CanvasTest.kt
+++ app/src/main/java/ru/mygames/customviewcompose/samples/CanvasTest.kt
@@ -5,10 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun CanvasTest(){
