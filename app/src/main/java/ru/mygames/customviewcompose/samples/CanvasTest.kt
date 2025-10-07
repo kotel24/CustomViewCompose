1bf26c3d3207c6e3f4fbc6d4d717ebf9fbb78e44
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-10-07 13:11:23 +0300

    готов к новой работе

--- app/src/main/java/ru/mygames/customviewcompose/samples/CanvasTest.kt
+++ app/src/main/java/ru/mygames/customviewcompose/samples/CanvasTest.kt
@@ -3,16 +3,10 @@ package ru.mygames.customviewcompose.samples
 import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.TileMode
-import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
