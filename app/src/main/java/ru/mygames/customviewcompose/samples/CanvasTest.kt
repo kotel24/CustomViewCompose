3c9f210091dab2f74aa96747f188b27dbc20b2e4
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-10-07 10:48:58 +0300

    drawPath - теперь работаю с этим объектом. Нарисовал звезду

--- app/src/main/java/ru/mygames/customviewcompose/samples/CanvasTest.kt
+++ app/src/main/java/ru/mygames/customviewcompose/samples/CanvasTest.kt
@@ -6,7 +6,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
+@Preview
 @Composable
 fun CanvasTest(){
     Canvas(
@@ -14,6 +20,22 @@ fun CanvasTest(){
             .fillMaxSize()
             .background(Color.Black)
     ) {
-        oleg()
+        drawPath(
+            path = Path().apply {
+                moveTo(x = center.x, y = 100.dp.toPx())
+                lineTo(x = center.x + 25.dp.toPx(), 150.dp.toPx())
+                lineTo(x = center.x + 75.dp.toPx(), 150.dp.toPx())
+                lineTo(x = center.x + 45.dp.toPx(), 200.dp.toPx())
+                lineTo(x = center.x + 60.dp.toPx(), 250.dp.toPx())
+                lineTo(x = center.x, 220.dp.toPx())
+                lineTo(x = center.x - 60.dp.toPx(), 250.dp.toPx())
+                lineTo(x = center.x - 45.dp.toPx(), 200.dp.toPx())
+                lineTo(x = center.x - 75.dp.toPx(), 150.dp.toPx())
+                lineTo(x = center.x - 25.dp.toPx(), 150.dp.toPx())
+                lineTo(x = center.x, 100.dp.toPx())
+            },
+            color = Color.White,
+            style = Fill
+        )
     }
 }
